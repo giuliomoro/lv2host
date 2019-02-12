3ba6b8a2b6aaa12ef48e8e7b1efb3c13447de654
adanlbenito <adanl.benito@gmail.com>
2019-02-12 16:45:42 +0000

    Custom render example with auto-generated slider gui (unrefined)

--- render.cpp
+++ render.cpp
@@ -1,8 +1,18 @@
 #include <Bela.h>
 #include <Lv2Host.h>
+#include <Gui.h>
 #include <math.h>
+#include <vector>
+#include <string.h>
 
 Lv2Host gLv2Host;
+Gui sliderGui;
+
+
+std::vector<std::vector<portDesc>> gPortDescriptions;
+std::vector<std::vector<int>> gControlPortNum;
+
+float gUpdateInterval = 0.05;
 
 void Bela_userSettings(BelaInitSettings *settings)
 {
@@ -11,8 +21,70 @@ void Bela_userSettings(BelaInitSettings *settings)
 	settings->analogOutputsPersist = 0;
 }
 
+void printPortDescription(const portDesc& description)
+{
+	printf("Port name: %s\n", description.name);
+	printf("Port type: %d\n", (int)description.type);
+	printf("Port min: %f\n", description.min);
+	printf("Port max: %f\n", description.max);
+	printf("Port default: %f\n", description.defaultVal);
+	printf("Port is logarithmic: %d\n", description.isLogarithmic);
+	printf("Port has strict bounds: %d\n", description.hasStrictBounds);
+}
+
+void createSliderFromDescription(unsigned int slotNumber, const portDesc& description)
+{
+	std::string sliderName;
+	sliderName = std::string(gLv2Host.getPluginName(slotNumber)) + " " + std::string(description.name);
+	float step = 0;
+	if(description.type == kToggle || description.type == kEnumerated || description.type == kInteger)
+		step = 1;
+		
+	sliderGui.addSlider(sliderName, description.min, description.max, step, description.defaultVal);
+}
+
+//getAllCtlPorts
+void generatePortDescriptions()
+{
+	for(unsigned int slot = 0; slot < gLv2Host.count(); slot++)
+	{
+		std::vector<portDesc> pluginPortDesc;
+		std::vector<int> controlPortNum;
+
+		for(unsigned int port = 0; port < gLv2Host.countPorts(slot); port++)
+		{
+			portDesc newPortDesc;
+			newPortDesc = gLv2Host.getPortDesc(slot, port);
+			if(newPortDesc.type != kNotControl)
+			{
+				
+				pluginPortDesc.emplace_back(newPortDesc);
+				controlPortNum.emplace_back(port);
+				createSliderFromDescription(slot,newPortDesc);
+			}
+		}
+		gPortDescriptions.emplace_back(pluginPortDesc);
+		gControlPortNum.emplace_back(controlPortNum);
+	}
+}
+
+void setControls()
+{
+	unsigned int portOffset = 0;
+	for(unsigned int slot = 0; slot < gControlPortNum.size(); slot++)
+	{
+		for(unsigned int cPort = 0; cPort < gControlPortNum[slot].size(); cPort ++)
+		{
+			gLv2Host.setPort(slot, gControlPortNum[slot][cPort], sliderGui.getSliderValue(cPort + portOffset));
+		}
+		portOffset = gControlPortNum[slot].size();
+	}
+	
+}
+	
 bool setup(BelaContext* context, void* userData)
 {
+	
 	// these should be initialized by Bela_userSettings above
 	if((context->flags & BELA_FLAG_INTERLEAVED) || context->audioSampleRate != context->analogSampleRate)
 	{
@@ -25,11 +97,9 @@ bool setup(BelaContext* context, void* userData)
 		fprintf(stderr, "Unable to create Lv2 host\n");
 		return false;
 	}
-	
+		
 	std::vector<std::string> lv2Chain;
-	lv2Chain.emplace_back("http://calf.sourceforge.net/plugins/Flanger");
-	lv2Chain.emplace_back("http://calf.sourceforge.net/plugins/Filter");
-	lv2Chain.emplace_back("http://calf.sourceforge.net/plugins/Limiter");
+	lv2Chain.emplace_back("http://calf.sourceforge.net/plugins/SidechainGate");
 	for(auto &name : lv2Chain)
 	{
 		gLv2Host.add(name);
@@ -39,18 +109,18 @@ bool setup(BelaContext* context, void* userData)
 		fprintf(stderr, "No plugins were successfully instantiated\n");
 		return false;	
 	}
-	// set effect parameter
-	if(int ret = gLv2Host.setPort(0, 7, -0.1))
-		fprintf(stderr, "error setting port for flanger: %d\n", ret);
-	if(int ret = gLv2Host.setPort(1, 5, 10.f))
-		fprintf(stderr, "error setting port for filter: %d\n", ret);
+
+	sliderGui.setup(5432, "gui");
+	generatePortDescriptions();
+	
+	//setControls();
 	
 	return true;
 }
 
 void render(BelaContext* context, void* userData)
 {
-	if(1)
+	if(0)
 	{
 		// modulate parameter once per block, for testing purposes
 		int tot = 5000;
@@ -61,8 +131,16 @@ void render(BelaContext* context, void* userData)
 		{
 			count = 0;
 		}
-		gLv2Host.setPort(1, 4, count);
 	}
+	
+	// Set control values 
+	static unsigned int count = 0;
+	for(unsigned int n = 0; n < context->audioFrames; n++) {
+				setControls();
+		count++;
+
+	}
+
 
 	// set inputs and outputs: normally you would just do this
 	const float* inputs[context->audioInChannels];
@@ -71,7 +149,10 @@ void render(BelaContext* context, void* userData)
 		inputs[ch] = (float*)&context->audioIn[context->audioFrames * ch];
 	for(unsigned int ch = 0; ch < context->audioOutChannels; ++ch)
 		outputs[ch] = &context->audioOut[context->audioFrames * ch];
-	if(1)
+		
+
+	
+	if(0)
 	{
 		// optionally, override audio inputs, for testing purposes
 		for(unsigned int n = 0; n < context->audioFrames; ++n)
@@ -92,3 +173,4 @@ void render(BelaContext* context, void* userData)
 }
 
 void cleanup(BelaContext* context, void* userData) {}
+	
